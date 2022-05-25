1889e8f211660a4e3718e18565446571f7ad6cde
Gerrie Dinga <gerriedinga@Gerries-MacBook-Pro.local>
2022-05-24 21:47:46 -0600

    new commit

---
+++ main.tf
@@ -0,0 +1,97 @@
+
+data "aws_availability_zones" "azs" {
+  state = "available" #it will look at the avail azs#
+}
+
+locals {
+  vpc_id = aws_vpc.kojitechs_vpc.id
+  mandatory_tag = {
+    line_of_business        = "hospital"
+    ado                     = "max"
+    tier                    = "WEB"
+    operational_environment = upper(terraform.workspace)
+    tech_poc_primary        = "udu.udu25@gmail.com"
+    tech_poc_secondary      = "udu.udu25@gmail.com"
+    application             = "http"
+    builder                 = "udu.udu25@gmail.com"
+    application_owner       = "kojitechs.com"
+    vpc                     = "WEB"
+    cell_name               = "WEB"
+    component_name          = "kojitechs"
+  }
+}
+
+#creating VPC
+resource "aws_vpc" "kojitechs_vpc" {
+
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true #to enable nat gateway
+  enable_dns_support   = true #to enable nat gateway
+
+  tags = {
+    "Name" = "kojitech_vpc"
+  }
+}
+
+#creating public subnet
+resource "aws_subnet" "pub_subnet_1" {
+  vpc_id                  = local.vpc_id
+  cidr_block              = var.pub_subnet_cidr[0]
+  availability_zone       = data.aws_availability_zones.azs.names[0]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "pub_subnet_1"
+  }
+}
+
+resource "aws_subnet" "pub_subnet_2" {
+  vpc_id                  = local.vpc_id
+  cidr_block              = var.pub_subnet_cidr[1]
+  availability_zone       = data.aws_availability_zones.azs.names[1]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "pub_subnet_2"
+  }
+}
+
+resource "aws_subnet" "priv_subnet_1" {
+  vpc_id            = local.vpc_id
+  cidr_block        = var.priv_subnet_cidr[0]
+  availability_zone = data.aws_availability_zones.azs.names[0]
+
+  tags = {
+    Name = "priv_subnet_${data.aws_availability_zones.azs.names[0]}" #appending the az name to the subn
+  }
+}
+
+resource "aws_subnet" "priv_subnet_2" {
+  vpc_id            = local.vpc_id
+  cidr_block        = var.priv_subnet_cidr[1]
+  availability_zone = data.aws_availability_zones.azs.names[1]
+
+  tags = {
+    Name = "priv_subnet_${data.aws_availability_zones.azs.names[1]}" #appending the az name to the subn
+  }
+}
+
+resource "aws_subnet" "database_subnet_1" {
+  vpc_id            = local.vpc_id
+  cidr_block        = var.database_subnet_cidr[0]
+  availability_zone = data.aws_availability_zones.azs.names[0]
+
+  tags = {
+    Name = "database_subnet_${data.aws_availability_zones.azs.names[0]}" #appending the az name to the subn
+  }
+}
+
+resource "aws_subnet" "database_subnet_2" {
+  vpc_id            = local.vpc_id
+  cidr_block        = var.database_subnet_cidr[1]
+  availability_zone = data.aws_availability_zones.azs.names[1]
+
+  tags = {
+    Name = "database_subnet_${data.aws_availability_zones.azs.names[1]}"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,40 @@
+
+variable "region" {
+  description = "AWS of region"
+  type        = string
+  default     = "us-east-1"
+
+}
+
+variable "vpc_cidr" {
+  description = "vpc name"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+# list [""]
+# componentize the subnets
+# -2 pub
+# -2 priv
+# -2 data
+
+variable "pub_subnet_cidr" {
+  description = "public subnet cidr"
+  type        = list(any)
+  default     = ["10.0.0.0/24", "10.0.2.0/24"]
+
+}
+
+variable "priv_subnet_cidr" {
+  description = "private subnet cidr"
+  type        = list(any)
+  default     = ["10.0.1.0/24", "10.0.3.0/24"]
+
+}
+
+variable "database_subnet_cidr" {
+  description = "database subnet cidr"
+  type        = list(any)
+  default     = ["10.0.51.0/24", "10.0.53.0/24"]
+
+}
